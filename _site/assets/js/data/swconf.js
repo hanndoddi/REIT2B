e730f918cfae7864ac255b86e91d8353653c984e
Þórarinn Bjartur Breiðfjörð <hanndoddi@gmail.com>
2024-08-26 21:38:51 +0000

    back in time

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,23 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1724708307',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+
+      
+      
+        '/assets/js/dist/app.min.js',
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
